591c7580861e36546fcf03f3e774695eddec1da0
praveena1996 <praveenaselva20@gmail.com>
2017-07-20 21:46:43 +0530

    Create phab.java

---
+++ phab.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class phab
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char h;
+BufferReader d=new BufferReader(new InputStream(System.in));
+h=(char)d.read;
+i(h='a'&&h='z'||h='A'&&h='Z')
+{
+System.out.print("alphabets");
+}
+else
+{
+System.out.print(" not alphabets");
+}
+}
+}
